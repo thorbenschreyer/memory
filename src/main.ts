00fc330d7c7ae9f692b3b67f29b354bc401df148
thorbenschreyer <thorben.schreyer@web.de>
2026-06-25 12:07:35 +0200

    add the base for theme design

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
+// ############### Other ###############
 import './styles/style.scss'
 import './styles/components/_button.scss'
 import './styles/layout/_flex.scss'
 import './styles/base/_all-pages.scss'
+
+// ############### Pages ###############
 import './styles/pages/_welcome-page.scss'
 import './styles/pages/_settings-page.scss'
+import './styles/pages/_gamefield-page.scss'
+
+// ############### Themes ###############
+import './styles/themes/_code-vibes-theme.scss'
+import './styles/themes/_gaming-theme.scss'
+import './styles/themes/_da-projects.scss'
+import './styles/themes/_foods-theme.scss.scss'
