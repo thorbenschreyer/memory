505120b95b81b40a07c447df17445c41b02d38b1
thorbenschreyer <thorben.schreyer@web.de>
2026-07-16 14:03:20 +0200

    implement the Dialog functions

--- src/game.ts
+++ src/game.ts
@@ -1,13 +1,65 @@
 let playerSetting: string;
 let numberOfCardsSetting: number;
+let currentPlayer: "orange" | "blue";
 
-const gamefieldTheme = document.getElementById('data-theme-gamefield')
+const dialog = document.getElementById("exit-dialog") as HTMLDialogElement;
+const exitGameButton = document.getElementById("exit-game-button")!;
+const backToGameButton = document.getElementById("back-to-game-button")!;
+const dialogExitGameButton = document.getElementById("quit-game-button")!;
 
 document.addEventListener("DOMContentLoaded", () => {
   const settings = JSON.parse(localStorage.getItem("settings") ?? "{}");
-   playerSetting = settings.player
-   numberOfCardsSetting = settings.numberOfCards
-   document.body.dataset.theme = settings.theme;
+  playerSetting = settings.player;
+  numberOfCardsSetting = settings.numberOfCards;
+  document.body.dataset.theme = settings.theme;
+
+  setStartingPlayer();
 });
 
+// Für den wechsel der Farbe müssen wir umbauen! Alles was Farben hat muss via klasse geändert werden können.
+// Daher muss dann bsp in gamefield-page zeile 89 umbauen zu Element __color und dann einen modifier --color hinzufügen
+function setStartingPlayer() {
+  if (playerSetting === "orange") {
+    currentPlayer = "orange";
+    console.log("Orange");
+  } else if (playerSetting === "blue") {
+    currentPlayer = "blue";
+    console.log("blue");
+  }
+}
 
+
+/* -------------------------------------------------------------------------- */
+/*                              Exit Dialog                                   */
+/* -------------------------------------------------------------------------- */
+
+/**
+ * Close dialog while clicking outside of themselfe
+ */
+dialog.addEventListener("click", (event) => {
+  if (event.target === dialog) {
+    dialog.close();
+  }
+});
+
+/**
+ * Show dialog while clicking on exit game
+ */
+exitGameButton?.addEventListener("click", (event) => {
+  dialog.showModal();
+});
+
+/**
+ * close dialog and go back to game 
+ */
+backToGameButton?.addEventListener("click", (event) => {
+  dialog.close();
+});
+
+/**
+ * close dialog, clear local storage and go to index.html
+ */
+dialogExitGameButton?.addEventListener("click", (event) => {
+  localStorage.clear();
+  window.location.href = "index.html"
+});
