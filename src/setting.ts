5da954652bf8d1a011245a0ec0cdb7b8ba6bccb4
thorbenschreyer <thorben.schreyer@web.de>
2026-07-16 12:04:48 +0200

    change image whyle hovering and save settings in local storage

--- src/setting.ts
+++ src/setting.ts
@@ -1,11 +1,19 @@
-const themeRadios = document.querySelectorAll<HTMLInputElement>('input[name="theme"]',);
-const playerRadios = document.querySelectorAll<HTMLInputElement>('input[name="player"]');
-const sizeRadios = document.querySelectorAll<HTMLInputElement>('input[name="size"]');
+const themeRadios = document.querySelectorAll<HTMLInputElement>(
+  'input[name="theme"]',
+);
+const themeHover = document.querySelectorAll<HTMLLabelElement>(".theme-label");
+const playerRadios = document.querySelectorAll<HTMLInputElement>(
+  'input[name="player"]',
+);
+const sizeRadios =
+  document.querySelectorAll<HTMLInputElement>('input[name="size"]');
 const themeDetails = document.getElementById("gaming-theme")!;
 const playerDetails = document.getElementById("starting-player")!;
 const numberOfCardsDetails = document.getElementById("card-size")!;
 const startButton = document.getElementById("start-button");
 const errorMassage = document.getElementById("error-massage")!;
+const themeImage = document.getElementById("theme-image") as HTMLImageElement;
+let usedThemeImage:string = "public/img/theme-it-logos.png";
 let theme: string;
 let player: string;
 let numberOfCards: number;
@@ -14,6 +22,28 @@ themeRadios.forEach((radio) => {
   radio.addEventListener("change", () => {
     theme = radio.value;
     updatetheme();
+  });
+});
+
+themeHover.forEach((element) => {
+  element?.addEventListener("mouseenter", () => {
+    const radioValueHover = element.querySelector<HTMLInputElement>('input[name="theme"]',);
+    if (!radioValueHover) return;
+    let hoveredElement = radioValueHover.value;
+
+    if (hoveredElement === "vibes") {
+        themeImage.src = "public/img/theme-it-logos.png";
+    } else if (hoveredElement === "gaming" ) {
+        themeImage.src = "public/img/theme-gameing.png";
+    } else if (hoveredElement === "project") {
+        themeImage.src = "public/img/theme-da-projects.png";
+    } 
+
+    console.log();
+  });
+
+  element.addEventListener("mouseleave", () => {
+    themeImage.src = usedThemeImage;
   });
 });
 
@@ -31,13 +61,36 @@ sizeRadios.forEach((radio) => {
   });
 });
 
+
+startButton?.addEventListener('click', ()=> {
+    const settings = {
+        theme, 
+        player,
+        numberOfCards
+    }
+
+    localStorage.setItem("settings", JSON.stringify(settings));
+});
+
+
+
+
+
+
+
 function updatetheme() {
   if (theme === "vibes") {
     themeDetails.innerText = "Code vibes theme";
+    usedThemeImage = "public/img/theme-it-logos.png";
+    themeImage.src = usedThemeImage;
   } else if (theme === "gaming") {
     themeDetails.innerText = "Gaming theme";
+    usedThemeImage = "public/img/theme-gameing.png";
+    themeImage.src = usedThemeImage;
   } else if (theme === "project") {
     themeDetails.innerText = "DA Projects theme";
+    usedThemeImage = "public/img/theme-da-projects.png";
+    themeImage.src = usedThemeImage;
   }
   checkStartValues();
 }
