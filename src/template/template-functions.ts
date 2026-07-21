a6df305416000036e0ceff76e95609237d514b48
thorbenschreyer <thorben.schreyer@web.de>
2026-07-22 00:44:37 +0200

    fix memory card arrays

--- src/template/template-functions.ts
+++ src/template/template-functions.ts
@@ -1,12 +1,23 @@
+import cardBackgroundCodeVibes from "../assets/image/theme-img/code-vibes/card-background-code-vibes.png";
+
 export function printCard(margin: string, imgPath: string): string {
   return `
-      <div class="card ${margin}">
-          <div class="card__inner">
-              <div class="card__face card__face--back"><img class="class__face--border" src=${imgPath} alt=""></div>
-              <div class="card__face card__face"><img class="card__face--img" src="../assets/image/theme-img/code-vibes/card-background-code-vibes.png" alt=""></div>
-          </div>
+    <div class="card ${margin}">
+      <div class="card__inner">
+        <div class="card__face card__face--back">
+          <img class="class__face--border" src="${imgPath}" alt="">
+        </div>
+
+        <div class="card__face card__face--front">
+          <img 
+            class="card__face--img" 
+            src="${cardBackgroundCodeVibes}" 
+            alt=""
+          >
+        </div>
       </div>
-    `;
+    </div>
+  `;
 }
 
 export function cardFieldColumn(index: number): string {
