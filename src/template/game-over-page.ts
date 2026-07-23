2ff391ab8f30e72e5159c4c0bacdd4ca11193bb9
thorbenschreyer <thorben.schreyer@web.de>
2026-07-23 14:35:38 +0200

    update semanthic

--- src/template/game-over-page.ts
+++ src/template/game-over-page.ts
@@ -1,13 +1,13 @@
 export function gameOverPage() {
   return `
-    <div class="gamefield-page game-over-page">
+    <section class="gamefield-page game-over-page">
     <div class="game-over-content">
-      <p class="game-over-header">GAME OVER</p>
+      <h1 class="game-over-header">GAME OVER</h1>
 
       <div class="score-section">
-        <p class="score-section_header">Final score</p>
-        <div class="score-section_overview">
-          <div class="header-menu__score--player">
+        <h2 class="score-section_header">Final score</h2>
+        <section class="score-section_overview">
+          <article class="header-menu__score--player">
             <div class="score-board__img score-board__img--blue"></div>
             <span class="score-section_header score-section_player--blue"
               >Blue </span
@@ -16,8 +16,8 @@ export function gameOverPage() {
               class="score-section_player--blue score-section_header"
               >6</span
             >
-          </div>
-          <div class="header-menu__score--player">
+          </article>
+          <article class="header-menu__score--player">
             <div class="score-board__img score-board__img--orange"></div>
             <span class="score-section_player--orange score-section_header"
               >Orange </span
@@ -26,10 +26,10 @@ export function gameOverPage() {
               class="score-section_player--orange score-section_header"
               >4</span
             >
-          </div>
-        </div>
+          </article>
+        </section>
       </div>
     </div>
-  </div>
+  </section>
     `;
 }
