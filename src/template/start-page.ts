082bee1fe3f5c7eb230d14207f12b0fd641ab66e
thorbenschreyer <thorben.schreyer@web.de>
2026-07-23 14:23:00 +0200

    update html start page

--- src/template/start-page.ts
+++ src/template/start-page.ts
@@ -5,10 +5,10 @@ import controllerWelcome from "../assets/image/controller_welcomepage.png";
 
 export function startPage() {
   return `
-    <div class="welcome-page flex">
+    <section class="welcome-page flex">
       <div class="weclome-page-text">
-        <span>It's play time.</span>
-        <p>Ready to play?</p>
+        <p>It's play time.</p>
+        <h1>Ready to play?</h1>
 
         <button class="button button--primary" id="play-button">
           <img class="button__controller" src="${controllerButton}" alt="Controller Icon" />
@@ -17,7 +17,7 @@ export function startPage() {
           <img class="button__arrow-hover" src="${arrowHover}" alt="Arrow Right Hover" />
         </button>
       </div>
-    </div>
+    </section>
 
     <img class="welcome-page-img" src="${controllerWelcome}" alt="Controller Icon" />
   `;
